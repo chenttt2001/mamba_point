e1aae86146945905ea40731515d0310e12fb397d
chenttt2001 <jiaqichen@jiaqideMacBook-Air.local>
2024-02-13 19:12:37 +0800

    v1.5

--- new_shell_c.sh
+++ new_shell_c.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH -A NAISS2023-5-359 -p alvis
 #SBATCH -t 1-00:00:00
-#SBATCH -w alvis6-08
-#SBATCH --gpus-per-node=A40:4
+#SBATCH -w alvis3-07
+#SBATCH --gpus-per-node=A100:2
 #SBATCH --job-name=test
 
 srun nvidia-smi
